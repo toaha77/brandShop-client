60a8488dc3b091c8dd061703fadc0c444bf6b422
toaha77 <istiaktoaha07@gmail.com>
2023-10-19 00:00:18 +0600

    add-product section added

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
+                  <img src="/car-logo-removebg-preview.png" className="h-8 mr-3" alt="FlowBite Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
               </a>
           </div>
